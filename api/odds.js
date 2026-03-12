147e932b089d867c3c8ef69cbce9fe5d1802b929
diegobet-ai <diegogiaffreda.smm@gmail.com>
2026-03-12 12:16:54 +0100

    Update odds.js

--- api/odds.js
+++ api/odds.js
@@ -6,7 +6,6 @@ export default async function handler(req, res) {
   const API_KEY = "cf8b575dd9664b4917486edb7c515b39";
 
   try {
-    // Richiedi tutti i mercati: 1X2, Over/Under, Handicap, BTTS
     const markets = "h2h,totals,spreads,btts";
     const url = `https://api.the-odds-api.com/v4/sports/${sport}/odds/?apiKey=${API_KEY}&regions=eu&markets=${markets}&oddsFormat=decimal`;
     const response = await fetch(url);
